839f081fb066a8cc7502047f160ce2fd39f65c3a
jinahkwak <wlsdk8949@naver.com>
2020-08-03 17:22:57 +0900

    result page

--- page/views.py
+++ page/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from page import views
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html',{'home':home})
+
+def rom(request):
+    return render(request,'rom.html',{'rom':rom})    
+
--- simlitest/settings.py
+++ simlitest/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
     'page',
 ]
 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -119,3 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'simlitest', 'static')
+ ]
+
+#STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+
+
