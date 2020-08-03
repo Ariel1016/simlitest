d7eb221a8ecc074314a286ba43d65eaf351e2870
wille318@naver.com <wille318@naver.com>
2020-08-03 18:39:22 +0900

    static관련 settings.py urls.py 코드 작성

--- simlitest/settings.py
+++ simlitest/settings.py
@@ -121,10 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'simlitest', 'static')
-]
-
+os.path.join(BASE_DIR, 'page', 'static') ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'page', 'media')
 MEDIA_URL = '/meida/'
